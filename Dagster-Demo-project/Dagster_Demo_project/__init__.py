9e83d9a7ef112c40c1ff2a66715d6c2b382fd83a
Melodie <fluxie97@gmail.com>
2023-12-04 18:55:19 +0100

    Update init file to add scheduling

--- Dagster-Demo-project/Dagster_Demo_project/__init__.py
+++ Dagster-Demo-project/Dagster_Demo_project/__init__.py
@@ -1,9 +1,23 @@
-from dagster import Definitions, load_assets_from_modules
-
+from dagster import (
+    load_assets_from_modules,
+    Definitions,
+    define_asset_job,
+    ScheduleDefinition,
+    with_resources
+)
 from . import assets
+import os
+from github import Github
 
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=all_assets,
+    schedules=[
+        ScheduleDefinition(
+            job=define_asset_job(name="daily_refresh", selection="*"),
+            cron_schedule="@daily",
+        )
+    ],
+    resources={"github_api": Github(os.environ["GITHUB_ACCESS_TOKEN"])}
 )
